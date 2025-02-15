f04565e081f9399533658510105b3a5589bc4376
Guyvinx <saintfleurguyvinx345@gmail.com>
2025-02-15 08:32:48 -0500

    Update set.js

--- set.js
+++ set.js
@@ -1,12 +1,12 @@
 const dotenv = require('dotenv');
 dotenv.config();
 module.exports = {
-  PREFIXE: process.env.PREFIXE || "",
-  NOM_OWNER: process.env.NOM_OWNER || "Ainz",
-  NUMERO_OWNER: process.env.NUMERO_OWNER || "",
+  PREFIXE: process.env.PREFIXE || "™",
+  NOM_OWNER: process.env.NOM_OWNER || "VINX",
+  NUMERO_OWNER: process.env.NUMERO_OWNER || "243858236454",
   MODE: process.env.MODE || "public",
-  MENU: process.env.MENU || "https://i.ibb.co/ynx9QcZ/image.jpg",
-  SESSION_ID: process.env.SESSION_ID || "ovl",
+  MENU: process.env.MENU || "https://files.catbox.moe/5j0n3s.jpg",
+  SESSION_ID: process.env.SESSION_ID || "Ovl-MD_ZEEfmFwr_SESSION-ID",
   LEVEL_UP: process.env.LEVEL_UP || "non",
   STICKER_PACK_NAME: process.env.STICKER_PACK_NAME || "Wa-sticker",
   STICKER_AUTHOR_NAME: process.env.STICKER_AUTHOR_NAME || "OVL-MD",
