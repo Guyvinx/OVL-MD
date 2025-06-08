0cee31ab8b408ddeab59ebb511170c7f41096e95
Guyvinx <saintfleurguyvinx345@gmail.com>
2025-06-08 09:49:51 -0400

    Update set.js

--- set.js
+++ set.js
@@ -1,21 +1,8 @@
-const dotenv = require('dotenv');
-dotenv.config();
-module.exports = {
-  PREFIXE: process.env.PREFIXE || "",
-  NOM_OWNER: process.env.NOM_OWNER || "Ainz",
-  NUMERO_OWNER: process.env.NUMERO_OWNER || "",
-  MODE: process.env.MODE || "public",
-  MENU: process.env.MENU || "https://i.ibb.co/ynx9QcZ/image.jpg",
-  SESSION_ID: process.env.SESSION_ID || "ovl",
-  LEVEL_UP: process.env.LEVEL_UP || "non",
-  STICKER_PACK_NAME: process.env.STICKER_PACK_NAME || "Wa-sticker",
-  STICKER_AUTHOR_NAME: process.env.STICKER_AUTHOR_NAME || "OVL-MD",
-  DATABASE: process.env.DATABASE || "postgresql://postgres.qnjvgxwyncnsbpfxwrbq:ovlmdmdpasse@aws-0-eu-central-1.pooler.supabase.com:6543/postgres",
-  ANTIDELETE: process.env.ANTIDELETE || "non",
-  RENDER_API_KEY: process.env.RENDER_API_KEY,
-  PRESENCE: process.env.PRESENCE || "off",
-  LIKE_STATUS: process.env.LIKE_STATUS || "off",
-  ANTI_VUE_UNIQUE: process.env.ANTI_VUE_UNIQUE || "off", 
-  DL_STATUS: process.env.DL_STATUS || "off",
-  LECTURE_STATUS: process.env.LECTURE_STATUS || "off",
-};
+PREFIXE=👑
+NOM_OWNER=𝙼𝙸𝙻𝙻𝙴 𝚅𝙸𝚂𝙰𝙶𝙴𝚂
+NUMERO_OWNER=50855760184
+MODE=public
+SESSION_ID=Ovl-MD_Lpq5MJuw_SESSION-ID
+LEVEL_UP=non
+STICKER_PACK_NAME=𝙼𝙸𝙻𝙻𝙴 𝚅𝙸𝚂𝙰𝙶𝙴𝚂
+STICKER_AUTHOR_NAME=𝙼𝙸𝙻𝙻𝙴 𝚅𝙸𝚂𝙰𝙶𝙴𝚂
